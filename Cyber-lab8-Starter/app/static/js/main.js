3f70adb474d4f75f068848d8a3ebadda5713651f
Turekkerem <keremozdogan@student.agh.edu.pl>
2026-01-14 23:43:46 +0100

    tryb ciemny

--- Cyber-lab8-Starter/app/static/js/main.js
+++ Cyber-lab8-Starter/app/static/js/main.js
@@ -1,7 +1,34 @@
 import { initDashboard } from './dashboard.js';
 import { initAdmin } from './admin.js';
 
+
+// Obsługa Dark Mode
+function initDarkMode() {
+    const btn = document.getElementById("darkModeBtn");
+    const body = document.body;
+    
+    // Sprawdź zapisane ustawienie
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-mode");
+    }
+
+    btn.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+        
+        // Zapisz do localStorage
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+    setAttribute(name, value)
+}
+
+
+
 function main() {
+    initDarkMode();
     const path = window.location.pathname;
 
     if (path === '/' || path === '/index') {
